4b2b63eac60e514143033215bcc020d54f805405
wschwetz <wschwetz1347@conestogac.on.ca>
2021-11-25 16:14:21 -0500

    startedtests

---
+++ TMSMainWindow/UnitTest/UnitTest1.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using TMSMainWindow;
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void CommTMSConstructorTest()
+        {
+            CommTMS obj = new CommTMS("root", "!Mustang123GT", "127.0.0.1", 3306, "tms");
+            Order newOrder = new Order("Malmart", "London", "Hamilton", 0, 0);
+            newOrder.Confirmed = 0;
+            int result = obj.InsertOrder(newOrder);
+            Assert.AreEqual(43, result);
+        }
+    }
+}
